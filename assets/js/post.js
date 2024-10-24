077125ca0ed1aad83b11593ba7836e319760eeed
Ariel Simulevski <ariel@simulevski.at>
2024-10-24 12:59:50 +0200

    Make responsive, add back button in post

--- assets/js/post.js
+++ assets/js/post.js
@@ -1,3 +1,7 @@
 document.querySelector(".shapes-triangle").addEventListener("click", async function() {
     window.location.href = "/";
 });
+
+document.querySelector(".back-button").addEventListener("click", async function() {
+    window.location.href = "/#blog";
+});
